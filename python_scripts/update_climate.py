4a592b6e0f4020645c7450a5fc553931e4bd74fc
Tobias Haase <tobhaase@gmail.com>
2020-03-10 22:04:27 +0100

    Add script

---
+++ python_scripts/update_climate.py
@@ -0,0 +1,55 @@
+DOMAIN = "climate"
+ENTITY_ID = data.get("entity_id", None)
+SENSORS_OFF = data.get("sensors_off", [])
+SENSORS_WINDOWS = data.get("windows", [])
+SENSOR_PRESENCE = data.get("sensor_presence", None)
+HEATING_FROM_HOUR = data.get("heating_from_hour", None)
+HEATING_TO_HOUR = data.get("heating_to_hour", None)
+SERVICE_DATA = {"entity_id": ENTITY_ID}
+
+
+def is_time_between(begin_time, end_time) -> bool:
+    check_time = dt_util.now().time()
+    if begin_time < end_time:
+        return check_time >= begin_time and check_time <= end_time
+    else:  # crosses midnight
+        return check_time >= begin_time or check_time <= end_time
+
+
+bool_off = False
+for sensor_off in SENSORS_OFF:
+    if hass.states.is_state(sensor_off, "off"):
+        bool_off = True
+for window in SENSORS_WINDOWS:
+    # We invert this statement to catch 'None' as well
+    if hass.states.is_state(window, "on"):
+        bool_off = True
+
+bool_presence = (
+    True
+    if SENSOR_PRESENCE is None
+    else not hass.states.is_state(SENSOR_PRESENCE, "off")
+)
+
+if bool_off:
+    # The heater should be off
+    logger.info("Set %s to Off", ENTITY_ID)
+    hass.services.call(DOMAIN, "turn_off", SERVICE_DATA, False)
+else:
+    # The heater should be on
+    if bool_presence and (
+        HEATING_FROM_HOUR is None
+        or HEATING_TO_HOUR is None
+        or is_time_between(
+            datetime.time(hour=HEATING_FROM_HOUR), datetime.time(hour=HEATING_TO_HOUR),
+        )
+    ):
+        # The heater should be in heating mode
+        logger.info("Set %s to Heat", ENTITY_ID)
+        SERVICE_DATA["hvac_mode"] = "heat"
+        hass.services.call(DOMAIN, "set_hvac_mode", SERVICE_DATA, False)
+    else:
+        # The heater should be in away mode
+        logger.info("Set %s to Heat Eco", ENTITY_ID)
+        SERVICE_DATA["preset_mode"] = "Heat Eco"
+        hass.services.call(DOMAIN, "set_preset_mode", SERVICE_DATA, False)
